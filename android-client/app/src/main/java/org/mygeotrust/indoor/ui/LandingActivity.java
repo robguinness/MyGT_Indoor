02af65f23f9e5c4a35ef2f5b6319162bbf665d90
Mesbahul Islam <mesbahul.cse.buet@gmail.com>
2016-06-14 11:31:11 +0300

    MyGT Indoor Landing Screen added and project set up to latest design library

---
+++ android-client/app/src/main/java/org/mygeotrust/indoor/ui/LandingActivity.java
@@ -0,0 +1,15 @@
+package org.mygeotrust.indoor.ui;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.fhc25.percepcion.osiris.mapviewer.R;
+
+public class LandingActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_landing);
+    }
+}
