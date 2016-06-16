cf572d52b0947f8320c0b0e177b4d5b6c0e1495d
Mesbahul Islam <mesbahul.cse.buet@gmail.com>
2016-06-16 15:47:17 +0300

    comments added

--- android-client/app/src/main/java/org/mygeotrust/indoor/tasks/loadIndoor/LoadIndoor.java
+++ android-client/app/src/main/java/org/mygeotrust/indoor/tasks/loadIndoor/LoadIndoor.java
@@ -45,11 +45,13 @@ public class LoadIndoor {
         applicationManager.getMetadataRepository().getMetadata(new ICallback<MetaData>() {
             @Override
             public void onFinish(Failure error, MetaData data) {
+                //TODO check Failure
                 LoadIndoor.this.metaData = data;
 
                 applicationManager.getBuildingsManager().getBuildings(PolygonCreator.createPolygon(metaData), new ICallback<BuildingGroup>() {
                     @Override
                     public void onFinish(Failure error, BuildingGroup data) {
+                        //TODO check Failure
                         observer.initFromViewState(internalStateManager.getViewState());
                     }
                 });
