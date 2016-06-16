addaced224a75d6e0b224a66adcb6af763be442b
Mesbahul Islam <mesbahul.cse.buet@gmail.com>
2016-06-16 10:37:56 +0300

    landing activity desined for loading map using mapsforge

--- android-client/app/src/main/java/org/mygeotrust/indoor/ui/LandingActivity.java
+++ android-client/app/src/main/java/org/mygeotrust/indoor/ui/LandingActivity.java
@@ -1,15 +1,56 @@
 package org.mygeotrust.indoor.ui;
 
-import android.app.Activity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import com.fhc25.percepcion.osiris.mapviewer.R;
+import com.fhc25.percepcion.osiris.mapviewer.ui.views.indoor.MapsforgeMapView;
 
-public class LandingActivity extends Activity {
+import org.mapsforge.map.android.graphics.AndroidGraphicFactory;
+
+import java.io.File;
+
+public class LandingActivity extends AppCompatActivity {
+
+    private MapsforgeMapView mapsforgeMapView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        AndroidGraphicFactory.createInstance(this.getApplication());
+
         setContentView(R.layout.activity_landing);
+
+        // setting the map view
+        mapsforgeMapView = (MapsforgeMapView) findViewById(R.id.map_view);
+        mapsforgeMapView.setMapFile(new File(""));
+        mapsforgeMapView.setMapPosition(60.161397, 24.738347);
+        mapsforgeMapView.setZoomLevel((byte) 16);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        switch (id){
+            case R.id.settings:
+                break;
+            case R.id.about:
+                break;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
